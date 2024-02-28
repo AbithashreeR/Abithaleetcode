d33ffbe8df20f9a8ddfc1c481d40a1b005353168
AbithashreeR <107352779+AbithashreeR@users.noreply.github.com>
2024-02-28 22:36:00 +0530

    Time: 13 ms (98.12%), Space: 71.3 MB (98.12%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -2,26 +2,32 @@ class Solution {
     public int maxWidthOfVerticalArea(int[][] points) {
        
         // ans=Arrays.sort(points.Comparator.comparingInt(a->a[0]));
-        List<Integer> res=new ArrayList<>();
-        for(int[] subArray:points)
+        // List<Integer> res=new ArrayList<>();
+        // for(int[] subArray:points)
+        // {
+        // res.add(subArray[0]);
+        // }
+        // Collections.sort(res);
+        int[] arr1=new int[points.length];
+        for(int i=0;i<points.length;i++)
         {
-        res.add(subArray[0]);
+           arr1[i]=points[i][0]; 
         }
-        Collections.sort(res);
-        int len=res.size();
+        Arrays.sort(arr1);
+        int len=arr1.length;
          int[] ans=new int[len];
         //diff
             
         for(int j=0;j<=len-2;j++)
         {
-            ans[j]=res.get(j+1)-res.get(j);
+            ans[j]=arr1[j+1]-arr1[j];
         }
         
         int max=0;
-        for(int i=0;i<ans.length;i++){
-           if(max<=ans[i])
+        for(int k=0;k<ans.length;k++){
+           if(max<=ans[k])
            {
-               max=ans[i];
+               max=ans[k];
            }
         }
         return max;
